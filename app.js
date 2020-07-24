1998b2c4dfa5f5d888b7809a94a9aec4a0297624
zhaoyuntong-good <crazy5017@163.com>
2020-07-24 14:36:52 +0800

    优化

--- app.js
+++ app.js
@@ -15,23 +15,23 @@ app.use(compress());
 
 var currentDir = path.posix.join(__dirname);
 
-//注册静态文件
-app.use('/web', express.static(path.join(__dirname, '/web')));
+//服务根路径为/，注册/web下的静态文件
+app.use('/', express.static(path.join(__dirname, '/web')));
 
 
-//主页请求
+//请求
 app.get('/', function(req, res) {
 	res.type('html');
 	res.sendFile(currentDir + '/web/index.html');
 });
 
 //设置代理 所有以/api路径的请求都被代理
-app.use('/api', proxy('http://10.50.7.122:8083', {
+app.use('/hccloud', proxy('http://192.168.1.238:8091', {
 	proxyReqPathResolver: function(req) {
-		return '/rcs' + url.parse(req.url).path;
+		return '/hccloud' + url.parse(req.url).path;
 	}
 }));
 
 app.listen(8880, function() {
-	console.log('server listening at 8880! wahahahaha!');
+	console.log('server listening at 8880!');
 });
